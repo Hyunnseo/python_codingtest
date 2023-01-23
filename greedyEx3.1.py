c1035e19b13f54c670897115ca8e58293827d016
Hyunnseo <tlsgustj110861@gmail.com>
2023-01-24 01:36:40 +0900

    p.92 more simple

--- greedyEx3.1.py
+++ greedyEx3.1.py
@@ -15,15 +15,19 @@ second = data[n-2]
 
 result = 0
 
-while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -= 1
-    if m == 0:
-        break
-    result += second
-    m -= 1
+# while True:
+#     for i in range(k):
+#         if m == 0:
+#             break
+#         result += first
+#         m -= 1
+#     if m == 0:
+#         break
+#     result += second
+#     m -= 1
+#
+# print(result)
+
+result = (k * first + second) * m//(k+1) + first * m%(k+1)
 
 print(result)
