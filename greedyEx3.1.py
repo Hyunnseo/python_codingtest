5a7e07c50d4c78f7178d24efd99dd8da3a848d15
Hyunnseo <tlsgustj110861@gmail.com>
2023-01-24 01:30:06 +0900

    p.92 code end

--- greedyEx3.1.py
+++ greedyEx3.1.py
@@ -13,4 +13,17 @@ data.sort()
 first = data[n-1]
 second = data[n-2]
 
+result = 0
 
+while True:
+    for i in range(k):
+        if m == 0:
+            break
+        result += first
+        m -= 1
+    if m == 0:
+        break
+    result += second
+    m -= 1
+
+print(result)
